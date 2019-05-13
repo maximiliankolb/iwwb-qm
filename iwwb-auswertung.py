3d13876a0c94e6369fa45c73234017803e1e685e
Maximilian <mail@maximilian-kolb.de>
2019-05-13 15:45:36 +0200

    Create iwwb-auswertung.py
    initial upload

---
+++ iwwb-auswertung.py
@@ -0,0 +1,22 @@
+# search files in iwwb/ for keywords
+# 2019-05-13 by maximilian
+
+import os
+
+keywords = ['Digitalisierung', 'Datenschutz', 'DSGVO', 'BWL', 'Finanz', 'Personal', 'EDV', 'Software', 'Betriebsrat', 'Betriebsräte', 'Arbeit 4.0', 'Industrie 4.0', 'Geschäftsführer', 'Führungskraft', 'Führungskräfte', 'Fachkraft', 'Fachkräfte']
+
+pathFolder = 'iwwb/'
+
+for fileName in os.listdir(pathFolder):
+	for keyword in keywords:
+		i = 0
+		fileHtml = pathFolder + fileName
+		searchfile = open(fileHtml, "r", encoding = "ISO8859-1")
+
+	for line in searchfile:
+		if keyword.casefold() in line.casefold():
+		# casefold to ignore upper and lower case
+			i = i+1
+		beautifulOutput = fileHtml + '_' + keyword + '_' + str(i)
+		print(beautifulOutput)
+		searchfile.close()
