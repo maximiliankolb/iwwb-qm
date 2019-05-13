9aa7870facb1f2f62bd499a29bbcc13144aaa0bd
Maximilian <mail@maximilian-kolb.de>
2019-05-13 15:16:12 +0200

    Create iwwb-download.py
    initial upload

---
+++ iwwb-download.py
@@ -0,0 +1,29 @@
+# parse json file to retrieve qm & download individual results to iwwb/
+# 2019-05-13 by maximilian
+
+import urllib.request
+from datetime import datetime
+import json
+import time
+
+pathJson = 'iwwb-source.json'
+pathFolder = 'iwwb/'
+
+json_file = open(pathJson, 'r')
+json_file_content = json_file.read()
+json_arrays = json.loads(json_file_content)
+
+for quali in json_arrays['qm']:
+	myFilenameDate = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+	myQmTitle = str(quali['qmTitel'])
+	myFilename = myFilenameDate + '_' + myQmTitle + '.html'
+	myDownloadPath = pathFolder + myFilename
+
+	myUrl = quali['qmQuelle']
+	print(myUrl, myDownloadPath)
+	time.sleep(1)
+
+	try:
+		urllib.request.urlretrieve(myUrl, myDownloadPath)
+	except Exception:
+		print ('error', myUrl)
