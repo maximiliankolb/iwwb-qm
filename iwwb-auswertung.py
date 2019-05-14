db15bdf76e588c2578c26470544f37016c258f28
Maximilian <mail@maximilian-kolb.de>
2019-05-14 13:23:13 +0200

    Update iwwb-auswertung.py
    add if statement to filter out qm * keyword combinations with less than five occurrences.

--- iwwb-auswertung.py
+++ iwwb-auswertung.py
@@ -17,6 +17,9 @@ for fileName in os.listdir(pathFolder):
 			if keyword.casefold() in line.casefold():
 			# casefold to ignore upper and lower case
 				i = i+1
-		beautifulOutput = fileHtml + '|' + keyword + '|' + str(i)
-		print(beautifulOutput[25:])
+		if i >= 5:
+			beautifulOutput = fileHtml + '|' + keyword + '|' + str(i)
+			print(beautifulOutput[25:])
+		else:
+			continue
 		searchfile.close()
