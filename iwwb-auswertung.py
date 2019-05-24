9bf605ec3ef890f0f541407d3e86b886a4a1cf59
Maximilian <mail@maximilian-kolb.de>
2019-05-24 10:01:40 +0200

    Update iwwb-auswertung.py
    change output to one line per title

--- iwwb-auswertung.py
+++ iwwb-auswertung.py
@@ -1,25 +1,29 @@
 # search files in iwwb/ for keywords
-# 2019-05-13 by maximilian
+# 2019-05-24 by maximilian
 
 import os
 
 keywords = ['Geschäftsführung', 'Geschäftsführer', 'Chef', 'Vorstand', 'Führungskraft', 'Führungskräfte', 'Geschäftsleitung', 'Unternehmen', 'Fachkraft', 'Fachkräfte', 'Facharbeiter', 'Mitarbeiter', 'Personal', 'Berufstätige', 'Betriebsrat', 'Betriebsräte', 'Mitbestimmung', 'Gewerkschaft', 'BWL', 'Finanz', 'Ökonomie', 'Pflege', 'Gesundheit', 'Krankenhaus', 'Digitalisierung', 'Industrie 4.0', 'Arbeit 4.0', 'Arbeiten 4.0', 'agil', 'EDV', 'Software', 'Office', 'Datenschutz', 'DSGVO', 'Patientendaten']
 
-pathFolder = 'iwwb/'
+pathFolder = 'iwwb-qm/'
 
 for fileName in os.listdir(pathFolder):
-	for keyword in keywords:
-		i = 0
-		fileHtml = pathFolder + fileName
-		searchfile = open(fileHtml, "r", encoding = "ISO8859-1")
+    list_numbers = ['']
 
-		for line in searchfile:
-			if keyword.casefold() in line.casefold():
-			# casefold to ignore upper and lower case
-				i = i+1
-		if i >= 5:
-			beautifulOutput = fileHtml + '|' + keyword + '|' + str(i)
-			print(beautifulOutput)
-		else:
-			continue
-		searchfile.close()
+    for keyword in keywords:
+        i = 0
+        fileHtml = pathFolder + fileName
+        searchfile = open(fileHtml, "r", encoding = "ISO8859-1")
+
+        for line in searchfile:
+            if keyword.casefold() in line.casefold():
+            # casefold to ignore upper and lower case
+                i = i+1
+
+        list_numbers.append(str(i))
+    numbers = '|'.join(list_numbers)
+    # separate numbers by '|'
+    beautifulOutput = fileName + numbers
+    print(beautifulOutput)
+
+    searchfile.close()
